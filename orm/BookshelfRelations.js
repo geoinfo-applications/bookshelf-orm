d0c771dec5ec0ec4543cb3281b68a814cfa8db47
Bruno Schäpper <bruno.schaepper@geoinfo.ch>
2015-11-06 13:57:41 +0100

    short-circuit wildcards if none or all are excluded and avoid to create relationNamesDeep and relationNamesDeepWithPrefixes multiple times

--- orm/BookshelfRelations.js
+++ orm/BookshelfRelations.js
@@ -7,6 +7,8 @@ class BookshelfRelations {
 
     constructor(Mapping) {
         this.Mapping = Mapping;
+        this.relationNamesDeep = this.getRelationNamesDeep();
+        this.relationNamesDeepWithPrefixes = this.getRelationNamesDeepWithPrefixes();
     }
 
     getFetchOptions(options) {
@@ -20,38 +22,45 @@ class BookshelfRelations {
     }
 
     applyExcludesToFetchProperties(fetchProperties, exclude) {
+        if (_.contains(exclude, "*")) {
+            fetchProperties.withRelated = [];
+            return;
+        }
+
         var wildcardExcludes = exclude.filter((e) => e.endsWith("*"));
         var excludes = _.difference(exclude, wildcardExcludes);
+        excludes = this.addWildcardExcludes(excludes, wildcardExcludes);
+        excludes = excludes.map(this.renameRelationProperty, this);
 
-        wildcardExcludes = wildcardExcludes.map((e) => e.substring(0, e.length - 1));
-        wildcardExcludes = this.relationNamesDeep.filter((relationName) => {
-            return wildcardExcludes.some((exclude) => relationName.startsWith(exclude));
-        });
+        fetchProperties.withRelated = _.reject(fetchProperties.withRelated, (name) => excludes.some((exclude) => name.indexOf(exclude) === 0));
+    }
 
-        excludes = _.union(excludes, wildcardExcludes).map(this.renameRelationProperty, this);
-
-        fetchProperties.withRelated = fetchProperties.withRelated.filter(property => {
-            return !excludes.some(exclude => property.indexOf(exclude) === 0);
-        });
+    addWildcardExcludes(excludes, wildcardExcludes) {
+        if (wildcardExcludes.length) {
+            wildcardExcludes = wildcardExcludes.map((e) => e.substring(0, e.length - 1));
+            wildcardExcludes = this.relationNamesDeep.filter((name) => wildcardExcludes.some((exclude) => {
+                return name.startsWith(exclude);
+            }));
+            excludes = _.union(excludes, wildcardExcludes);
+        }
+        return excludes;
     }
 
     renameRelationProperty(propertyName) {
         var isRelationName = _.contains(this.relationNamesDeep, propertyName);
 
         if (isRelationName) {
-            return propertyName.split(".").map(function (namePart) {
-                return "relation_" + namePart;
-            }).join(".");
+            return propertyName.split(".").map((name) => "relation_" + name).join(".");
         }
 
         return propertyName;
     }
 
     get relationNames() {
-        return _.map(this.Mapping.relations, relation => relation.name);
+        return _.map(this.Mapping.relations, (relation) => relation.name);
     }
 
-    get relationNamesDeep() {
+    getRelationNamesDeep() {
         function extractName(parent, relation) {
             var name = _.compact([parent, relation.name]).join(".");
             return [name].concat(_.map(relation.references.mapping.relations, _.partial(extractName, name)));
@@ -60,12 +69,8 @@ class BookshelfRelations {
         return _.flatten(_.map(this.Mapping.relations, _.partial(extractName, "")));
     }
 
-    get relationNamesDeepWithPrefixes() {
-        return this.relationNamesDeep.map(function (relationName) {
-            return relationName.split(".").map(function (name) {
-                return "relation_" + name;
-            }).join(".");
-        });
+    getRelationNamesDeepWithPrefixes() {
+        return this.relationNamesDeep.map((relationName) => relationName.split(".").map((name) => "relation_" + name).join("."));
     }
 
     get fetchProperties() {
